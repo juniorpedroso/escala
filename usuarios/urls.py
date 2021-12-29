c45b2a359012e15d3fa31b73f212ae4639ed38c5
Junior Pedroso <sintasebemagora@gmail.com>
2021-12-29 13:31:22 -0300

    cadastro
    Incluido cadastrl de usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,14 @@
 '''Define padrões de URL para usuarios'''
 
 from django.urls import path, include
+from . import views
 
 app_name = 'usuarios'
 urlpatterns = [
     # Página de login
     path('', include('django.contrib.auth.urls')),
+
+    # Página de cadastro
+    path('register/', views.register, name='register'),
 
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    '''Cadastra um novo usuário.'''
+    if request.method != 'POST':
+        # Exibe um formulário em branco
+        form = UserCreationForm()
+    else:
+        # Processa o formulário preenchido
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Faz login do usuário e o redireciona para a página principal
+            login(request, new_user)
+            return redirect('escalas:index')
+    # Exibe um formulário em branco ou inválido
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
+    
+
+
+
